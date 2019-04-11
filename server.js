d74356b0899b170a88efc87ad28d551d4fc59a1b
Devin Kapla <devinkapla@gmail.com>
2019-04-11 09:30:09 -0600

    Psuedo code new user and preferences

--- server.js
+++ server.js
@@ -4,8 +4,26 @@ import cors from 'cors';
 app.use(express.json());
 app.use(cors());
 
-app.locals.users = { userName: 'David', id: 1 }
+app.locals.users = [{ userName: 'David', id: 1 }];
+app.locals.preferences = [{}, {}];
 
-app.listen(3001, () => console.log(`App listening on port 3001!`))
+app.listen(3001, () => console.log(`App listening on port 3001!`));
 
-app.get('/', (req, res) => res.status(200).json(app.locals.users))
+app.get('/signin', (req, res) => {
+  // match username and password and return happy or sad path
+  res.status(200).json(app.locals.users[0]);
+})
+
+app.get('/preferences', (req, res) => {
+  // mach user id to prefrences and return happy or sad path
+})
+
+app.post('/newUser', (req, res) => {
+  // check username and password for use
+  // create new user or return sad path
+})
+
+app.post('/addPreferences', (req, res) => {
+  // check user id for use
+  // create new preferences  or return sad path
+})
