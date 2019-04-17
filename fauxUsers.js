37518868bc9828c4482e577f6a3c1f155e639409
Devin Kapla <devinkapla@gmail.com>
2019-04-17 07:19:36 -0600

    Change zip and locs in data

--- fauxUsers.js
+++ fauxUsers.js
@@ -1,6 +1,6 @@
 export const users = [
   { email: 'Christie', userName: 'Christie', password: 'twelve', id: 1, zip: 80204, lat: 39.734851, lng: -105.020299 },
   { email: 'Mike', userName: 'Mike', password: 'finn', id: 2, zip: 99950, lat: 55.339989, lng: -131.639916 },
-  { email: 'Justin', userName: 'Justin', password: 'noodles', id: 3, zip: 80204, lat: 39.734851, lng: -105.020299 },
-  { email: 'Kayla', userName: 'Kayla', password: 'vortex', id: 4, zip: 80204, lat: 39.734851, lng: -105.020299 },
+  { email: 'Justin', userName: 'Justin', password: 'noodles', id: 3, zip: 99501, lat: 61.22246, lng: -149.86769 },
+  { email: 'Kayla', userName: 'Kayla', password: 'vortex', id: 4, zip: 96701, lat: 19.8968, lng: 155.5828 },
 ]
